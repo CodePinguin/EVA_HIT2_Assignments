07de63231482562b02b584370f3d9ab2e36bd962
Celina Vetter <celinavetter@online.de>
2021-04-12 23:28:02 +0200

    delete Würfel, which helped for KOS

--- CG-01_A.01_Assignment_FlugSim/src/FlugSim.cpp
+++ CG-01_A.01_Assignment_FlugSim/src/FlugSim.cpp
@@ -46,16 +46,13 @@ string MENU_ENTRY_STR[4];
 const float WIDTH = 2.0f;
 
 // VAO
-GLuint VAO[2];
+GLuint VAO;
 
 // TODO: add additional globals for shaft indices count
 GLuint GROUND_INDICES_COUNT = 0;
 GLuint SHAFT_INDICES_COUNT = 0;
 GLuint64 SHAFT_DRAW_OFFSET = 0;
 
-GLuint HOUSE_INDICES_COUNT = 0;
-GLuint ROOF_INDICES_COUNT = 0;
-GLuint64 ROOF_DRAW_OFFSET = 0;
 
 Aircraft aircraft;
 
@@ -112,63 +109,18 @@ void initModel(float width)
     };
     SHAFT_INDICES_COUNT = sizeof(shaft_indices) / sizeof(shaft_indices[0]);
 
-    float test1 = 5;
-    float test2 = 4;
-    GLfloat house_vertices[] =
-    {
-        -test1, -test1, test1, 1.0f, //v0
-        -test2, -test1, test1, 1.0f, //v1
-        -test2, -test1, test2, 1.0f, //v2
-        -test1, -test1, test2, 1.0f, //v3
-
-        -test1, -test2, test1, 1.0f, //v4
-        -test2, -test2, test1, 1.0f, //v5
-        -test2, -test2, test2, 1.0f, //v6
-        -test1, -test2, test2, 1.0f, //v7
-    };
-
-    GLfloat house_colors[] =
-    {
-        1.0f, 1.0f, 1.0f, //v0 
-        1.0f, 0.0f, 1.0f, //v1
-        0.0f, 1.0f, 0.0f, //v2
-        0.0f, 0.0f, 1.0f, //v3
-         
-        1.0f, 1.0f, 1.0f, //v4
-        1.0f, 0.0f, 1.0f, //v5
-        0.0f, 1.0f, 0.0f, //v6
-        0.0f, 0.0f, 1.0f, //v7
-    };
-
-    GLushort house_indices[] =
-    {
-        0, 3, 2,
-        1, 0, 2,
-        0, 4, 5,
-        1, 0, 5,
-        1, 5, 6,
-        2, 1, 6,
-        2, 6, 7,
-        3, 2, 7,
-        3, 7, 4,
-        0, 3, 4,
-        4, 7, 6,
-        5, 4, 6
-    };
-    HOUSE_INDICES_COUNT = sizeof(house_indices) / sizeof(house_indices[0]);
-    ROOF_DRAW_OFFSET = sizeof(house_indices);
 
 
     // TODO: create two global VAOs for seperate ground/shaft and house/roof drawing //////////////
-    glGenVertexArrays(2, &VAO[0]);
+    glGenVertexArrays(1, &VAO);
 
     // TODO: create two local VBOs for seperate ground/shaft and house/roof setup
-    GLuint vbo[2];
-    glGenBuffers(2, &vbo[0]);
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
 
     // TODO: create two local EBOs for seperate ground/shaft and house/roof setup
-    GLuint ebo[2];
-    glGenBuffers(2, &ebo[0]);
+    GLuint ebo;
+    glGenBuffers(1, &ebo);
 
     // get position and color vertex attribute locations (requires compiled shader program!)
     GLuint vecPosition = glGetAttribLocation(PROGRAM_ID, "vecPosition");
@@ -176,10 +128,10 @@ void initModel(float width)
 
 
     // bind VAO for ground/shaft setup ////////////////////////////////////////////////////////////
-    glBindVertexArray(VAO[0]);
+    glBindVertexArray(VAO);
 
     // setup VBO for ground/shaft position and color vertex attributes
-    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(ground_vertices) + sizeof(ground_colors), nullptr, GL_STATIC_DRAW);
     glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(ground_vertices), ground_vertices);
     glBufferSubData(GL_ARRAY_BUFFER, sizeof(ground_vertices), sizeof(ground_colors), ground_colors);
@@ -191,35 +143,10 @@ void initModel(float width)
     glEnableVertexAttribArray(COLOR_VEC3_LOCATION);
 
     // setup EBO for ground/shaft face indices
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo[0]);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(ground_indices) + sizeof(shaft_indices), nullptr, GL_STATIC_DRAW);
     glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, sizeof(ground_indices), ground_indices);
     glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, SHAFT_DRAW_OFFSET, sizeof(shaft_indices), shaft_indices);
-
-
-
-
-    glBindVertexArray(VAO[1]);
-
-    // TODO: setup VBO for house/roof position and color vertex attributes
-    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(house_vertices) + sizeof(house_colors), nullptr, GL_STATIC_DRAW);
-    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(house_vertices), house_vertices);
-    glBufferSubData(GL_ARRAY_BUFFER, sizeof(house_vertices), sizeof(house_colors), house_colors);
-
-    // define color vertex attribute default draw color if array is disabled
-    //glVertexAttrib3f(COLOR_VEC3_LOCATION, 1.0f, 1.0f, 0.0f);
-
-    // TODO: define house/roof vertex attributes data format and enable them
-    glVertexAttribPointer(vecPosition, 4, GL_FLOAT, GL_FALSE, 0, GL_BUFFER_OFFSET(0));
-    glEnableVertexAttribArray(vecPosition);
-    glVertexAttribPointer(COLOR_VEC3_LOCATION, 3, GL_FLOAT, GL_FALSE, 0, GL_BUFFER_OFFSET(sizeof(house_vertices)));
-    glEnableVertexAttribArray(COLOR_VEC3_LOCATION);
-
-    // TODO: setup EBO for house/roof face indices
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo[1]);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(house_indices), nullptr, GL_STATIC_DRAW);
-    glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, sizeof(house_indices), house_indices);
 
 }
 
@@ -240,7 +167,7 @@ void glutDisplayCB(void)
     glUniformMatrix4fv(MODELVIEW_MAT4_LOCATION, 1, GL_FALSE, glm::value_ptr(mouse));
 
     // bind VAO to current drawing context
-    glBindVertexArray(VAO[0]);
+    glBindVertexArray(VAO);
 
     //glm::mat4 modelview = aircraft.GetModelView();
     //modelview = modelview * mouse;
@@ -251,23 +178,6 @@ void glutDisplayCB(void)
 
 
     glUniformMatrix4fv(MODELVIEW_MAT4_LOCATION, 1, GL_FALSE, glm::value_ptr(mouse));
-
-    // TODO: add indexed drawing of house/roof
-    // bind house/roof VAO to current drawing context
-    glBindVertexArray(VAO[1]);
-
-    // disable color vertex attribute to set default color for house drawing
-    //glDisableVertexAttribArray(COLOR_VEC3_LOCATION);
-
-    // indexed drawing of house
-    glDrawElements(GL_TRIANGLE_STRIP, HOUSE_INDICES_COUNT, GL_UNSIGNED_SHORT, nullptr);
-
-    // re-enable color vertex attribute to use color array for roof drawing
-    //glEnableVertexAttribArray(COLOR_VEC3_LOCATION);
-
-    // indexed drawing of roof
-    glDrawElements(GL_TRIANGLE_FAN, ROOF_INDICES_COUNT, GL_UNSIGNED_SHORT, GL_BUFFER_OFFSET(ROOF_DRAW_OFFSET));
-
 
     glutSwapBuffers();
     UtilOpenGL::checkOpenGLErrorCode();
